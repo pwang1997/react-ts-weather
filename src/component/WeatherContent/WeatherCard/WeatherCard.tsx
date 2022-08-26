9d3db668970dedec89cf5bc88c6d82ffda44b72d
zwang684 <puck19970418@gmail.com>
2022-08-26 16:15:51 -0400

    Added google geocoding for fetching city name

--- src/component/WeatherContent/WeatherCard/WeatherCard.tsx
+++ src/component/WeatherContent/WeatherCard/WeatherCard.tsx
@@ -4,7 +4,7 @@ import Card from "@mui/material/Card";
 import * as React from "react";
 import {ILocation} from "../../../types/interfaces";
 import {useEffect, useState} from "react";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material";
@@ -17,7 +17,8 @@ interface IProps {
 export default function WeatherCard({location}: IProps) {
     const [forecastList, setForecastList] = useState();
     const [tempList, setTempList] = useState(Array());
-    const [expanded, setExpanded] = React.useState<string | false>(false);
+    const [expanded, setExpanded] = useState<string | false>(false);
+    const [city, setCity] = useState();
 
     const handleChange =
         (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
@@ -26,19 +27,41 @@ export default function WeatherCard({location}: IProps) {
 
     useEffect(() => {
         const count = 2;
-        axios.get(`https://api.openweathermap.org/data/2.5/forecast?units=metric&cnt=${count}&lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_WEATHER_API}`)
-            .then(res => {
-                const data = res.data.list;
-                setForecastList(data);
+        const fetch_weather_url = `https://api.openweathermap.org/data/2.5/forecast?units=metric&cnt=${count}&lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_WEATHER_API}`;
+        const fetch_city_url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.latitude}, ${location.longitude}&result_type=locality&key=${process.env.REACT_APP_GOOGLE_GEOLOCATION_API}`;
 
-                for (const forecast of data) {
-                    setTempList([...tempList, {
-                        time: forecast.dt_txt,
-                        temp: forecast.main.temp,
-                        rain_chance: (Object.values(forecast.rain)[0] as number) * 100 + "%"
-                    }]);
-                }
-            })
+        const reqFetchWeather = axios.get(fetch_weather_url);
+        const resFetchWeather = (res: AxiosResponse) => {
+            const data = res.data.list;
+            setForecastList(data);
+
+            let forecastList = Array<any>();
+            for (const forecast of data) {
+                forecastList = [...forecastList, {
+                    time: forecast.dt_txt,
+                    temp: forecast.main.temp
+                }];
+            }
+            setTempList([...tempList, ...forecastList]);
+        }
+
+        const reqFetchCity = axios.get(fetch_city_url);
+        const resFetchCity = (res: AxiosResponse) => {
+            setCity(res.data.results[0].formatted_address);
+        }
+
+        axios.all([reqFetchWeather, reqFetchCity])
+            .then(axios.spread((...res) => {
+                // axios responses
+                const resWeather = res[0];
+                const resCity = res[1];
+                // update States
+                resFetchWeather(resWeather);
+                resFetchCity(resCity);
+            }))
+            .catch(err => {
+                console.log(`ERROR(${err.code}): ${err.message}`)
+            });
     }, []);
     return (
         <Card className="center">
@@ -51,6 +74,11 @@ export default function WeatherCard({location}: IProps) {
                 <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
                     Longitude: {
                     location?.longitude
+                }
+                </Typography>
+                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                    Location: {
+                    city
                 }
                 </Typography>
             </CardContent>
@@ -72,9 +100,6 @@ export default function WeatherCard({location}: IProps) {
                                 <AccordionDetails>
                                     <Typography>
                                         Temperature : {data.temp}
-                                    </Typography>
-                                    <Typography>
-                                        Raining Chance: {data.rain_chance}
                                     </Typography>
                                 </AccordionDetails>
                             </Accordion>
