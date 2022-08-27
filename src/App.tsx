3ef2bb426df34f168f43a566dae6802250546d6b
zwang684 <puck19970418@gmail.com>
2022-08-27 00:44:56 -0400

    Added searchbar for fetching more cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import CustomizedInputBase from "./component/SearchBar/SearchBar";
+import SearchBar from "./component/SearchBar/SearchBar";
 import WeatherContent from "./component/WeatherContent/WeatherContent";
+import {ILocation} from "./types/interfaces";
 
 function App() {
+    const [locationList, setLocationList] = useState<ILocation[]>([]);
+
+    const onUpdateLocationList = (location: ILocation) => {
+        setLocationList([...locationList, location]);
+        console.log(locationList);
+    }
+
     return (
         <div className="App">
             <div>
-                <CustomizedInputBase />
+                <SearchBar onUpdateLocationList={onUpdateLocationList}/>
             </div>
 
             <div>
--- src/component/SearchBar/SearchBar.tsx
+++ src/component/SearchBar/SearchBar.tsx
@@ -1,19 +1,67 @@
-import * as React from 'react';
-import {useState} from "react";
-// import {ILocation} from "../../types/interfaces";
+import React, {FC, useState} from "react";
+import {Button, TextField} from "@mui/material";
+import axios from "axios";
 
-export default function CustomizedInputBase() {
-    // const [location, setLocation] = useState<null | ILocation>(null);
+import './styles.css';
+import {ILocation} from "../../types/interfaces";
+import useDebounce from "../../hook/CmHook";
 
-    const [city, setCity] = useState<null | string>(null);
+interface IProps {
+    onUpdateLocationList : (loc: ILocation) =>  void;
+}
 
-    const handleSearchBar = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        setCity(e.target.value);
+const SearchBar: FC<IProps> = ({onUpdateLocationList} : IProps) => {
+
+    const [location, setLocation] = useState<null | ILocation>(null);
+    const [debouncedCity, city, setCity] = useDebounce<string>("", 500);
+    const [data, setData] = useState<string[]>([]);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const newCity = e.target.value;
+        setCity(newCity);
+        if(!Boolean(debouncedCity)) {
+            return;
+        }
+        const fetch_city_api = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.REACT_APP_GOOGLE_GEOLOCATION_API}&address=${newCity}`;
+        axios.get(fetch_city_api)
+            .then(res => {
+                if(res.data.length !== 0) {
+                    const result = res.data.results[0];
+
+                    setLocation({
+                        latitude : result.geometry.location.lat,
+                        longitude : result.geometry.location.lng
+                    });
+                    setData([...data, result.formatted_addresss]);
+                    console.log(result);
+                }
+            })
+            .catch(err => {
+                console.warn(`ERROR(${err.code}): ${err.message}`);
+            });
     }
 
+    const handleSubmit = (event: React.MouseEvent<HTMLElement>) => {
+        if(location !== null) {
+            onUpdateLocationList(location as ILocation);
+            setCity("");
+            setLocation(null);
+        }
+        event.preventDefault();
+    }
     return (
-       <form>
-           <input type={"search"} onChange={handleSearchBar}/>
-       </form>
+        <div className="search-container">
+            <TextField
+                id="search-input"
+                className="search-input"
+                label="search"
+                value={city}
+                onChange={handleSearch}
+            />
+            <Button type="submit"
+                    variant="contained"
+                    onClick={handleSubmit}>Search</Button>
+        </div>
     );
 }
+export default SearchBar;
